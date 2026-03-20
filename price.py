90f2de2d38e590595b88e134a401b2212ae763b3
markusbaltzer <markus.baltzer.nielsen@gmail.com>
2026-03-20 19:29:47 +0100

    added simulation and efficiency test

--- price.py
+++ price.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 def max_profit_robust(prices):
     if len(prices) < 2:
         return None
@@ -9,10 +12,21 @@ def max_profit_robust(prices):
         min_price = min(min_price, price)
     return max_profit
 
-T1 = [10, 7, 5, 8, 11, 9]
+T1 = [2, 7, 5, 8, 11, 9]
 T4 = [6, 5, 4, 3, 2, 1]
 T3 = [1,1,1,1,1,1,1]
 
 print(max_profit_robust(T1))
 print(max_profit_robust(T4))
 print(max_profit_robust(T3))
+
+#add simulation check
+
+large_prices = [random.randint(1, 100) for i in range(10000000)]
+
+start_time = time.time()
+result = max_profit_robust(large_prices)
+end_time = time.time()
+
+print(result)
+print(end_time-start_time)
