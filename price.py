49ffb3fc786cd0e16594d02fe57221f5cd8471ea
Markus Baltzer <markusbaltzer@MacBook-Air-tilhrende-Markus.local>
2026-03-20 18:21:13 +0100

    Coded the stock max profit from R to python

---
+++ price.py
@@ -0,0 +1,18 @@
+def max_profit_robust(prices):
+    if len(prices) < 2:
+        return None
+    min_price = prices[0]
+    max_profit = float('-inf')
+    for price in prices[1:]:
+        current_profit = price - min_price
+        max_profit = max(max_profit, current_profit)
+        min_price = min(min_price, price)
+    return max_profit
+
+T1 = [10, 7, 5, 8, 11, 9]
+T4 = [6, 5, 4, 3, 2, 1]
+T3 = [1,1,1,1,1,1,1]
+
+print(max_profit_robust(T1))
+print(max_profit_robust(T4))
+print(max_profit_robust(T3))
